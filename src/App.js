f8048df4e7ec29c89e20a9ca8d8a3e08adc04bc6
Samuel Desbos <samuel.desbos@gmail.com>
2020-01-13 17:11:41 +0100

    Section 4 : Working with Lists and Conditionals

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import UserOutput from './User/UserOutput';
-import UserInput from './User/UserInput';
 
 // container / stateful / smart component (they use state)
 class App extends Component {
   state = {
     persons: [
-      { name: 'Sam', age: 35 },
-      { name: 'Toto', age: 43 }
+      { id: "0001", name: 'Sam', age: 35 },
+      { id: "0002", name: 'Toto', age: 43 },
+      { id: "0003", name: 'Sarah', age: 22 },
     ],
     otherState: 'other value',
-    userName: 'Bob'
+    userName: 'Bob',
+    showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('was clicked');
-    // DONT : this.state.persons[0].name = 'Bob';
-    this.setState({
-      persons: [
-        { name: newName, age: 35 },
-        { name: 'Toto', age: 43 }
-      ],
-    });
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: event.target.value, age: 35 },
-        { name: 'Toto', age: 43 }
-      ],
+  nameChangeHandler = (event, id) => {
+    console.log('event', event);
+    console.log('id',id);
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    console.log('person', person);
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   }
 
-  userNameChangeHandler = (event) => {
-    this.setState({
-      userName: event.target.value
-    });
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
@@ -51,28 +57,33 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              changed={(event) => this.nameChangeHandler(event, person.id)}
+              name={person.name}
+              age={person.age}
+              key={person.id} />
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hello world</h1>
         <p>This is working</p>
         <button
           style={style}
-          onClick={() => this.switchNameHandler('John')}>Switch name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler.bind(this, 'Jack')}
-        changed={this.nameChangeHandler}>
-          My hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age} />
+          onClick={this.togglePersonsHandler}>Toggle persons</button>
 
-          <UserInput name={this.state.userName}
-          changed={this.userNameChangeHandler} />
-          <UserOutput userName={this.state.userName} />
-          <UserOutput userName="Paul" />
+          {persons}
+
       </div>
     );
     // we have 2 ways to bind argument to switchNameHandler
